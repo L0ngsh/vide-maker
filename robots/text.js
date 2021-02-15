24943aac6f6a097a08f25da411a266806389128c
L0ngsh <gilbersoncfjunior@gmail.com>
2021-02-15 02:37:47 -0300

    feat: add watson Natural Language Understanding

--- robots/text.js
+++ robots/text.js
@@ -1,12 +1,24 @@
 const algorithmia = require('algorithmia')
 const sbd = require('sbd')
+const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+const { IamAuthenticator } = require('ibm-watson/auth');
 
 const algorithmiaApiKey = require('../credentials/algorithmia.json').apiKey
+const watsonApiKey = require('../credentials/watson-nlu.json').apikey
+const watsonUrl = require('../credentials/watson-nlu.json').url
+
+const nlu = new NaturalLanguageUnderstandingV1({
+    authenticator: new IamAuthenticator({ apikey: watsonApiKey }),
+    version: '2018-04-05',
+    serviceUrl: watsonUrl
+})
 
 async function robot(content) {
     await fetchContentFromWikipedia(content)
     sanitializeContent(content)
     breakContentntoSentences(content)
+    limitmaximumSentences(content)
+    await fetchKeywordsOfAllSentences(content)
 
     async function fetchContentFromWikipedia(content) {
         const algorithmiaAuthenticated = algorithmia.client(algorithmiaApiKey)
@@ -54,6 +66,32 @@ async function robot(content) {
                 images: []
             })
         })
+    }
+
+    function limitmaximumSentences(content) {
+        content.sentences = content.sentences.slice(0, content.maximumSentences)
+    }
+
+    async function fetchKeywordsOfAllSentences(content) {
+        for (const sentence of content.sentences) {
+            sentence.keyword = await fetchWatsonAndReturnKeywords(sentence.text)
+        }
+    }
+
+    async function fetchWatsonAndReturnKeywords(sentence) {
+        const response = await nlu.analyze(
+            {
+              text: sentence,
+              features: {
+                keywords: {}
+              }
+            })
+    
+        const keywords = response.result.keywords.map((keyword) => {
+            return keyword.text
+        })
+    
+        return keywords
     }
 }
 
